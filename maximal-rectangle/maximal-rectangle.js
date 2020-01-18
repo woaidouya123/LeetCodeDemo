0bf4f0b7693b2845075e29823d23f193f50bfbeb
woaidouya123 <cfeigood@qq.com>
2020-01-18 23:48:59 +0800

    最大矩形调整

--- maximal-rectangle/maximal-rectangle.js
+++ maximal-rectangle/maximal-rectangle.js
@@ -14,41 +14,50 @@
 */
 /**
  * 思路：
- * 采用动态规划的思想，创建保存每个位置能出现的最大矩形的宽高的数组
- * 遍历数组，计算该位置最大矩形的宽高，计算方法如下：
- *     如果该位置数字为1：
- *         获取正上方数字的最大矩形宽高w1/h1,左面数字的最大矩形宽高w2/h2
- *         当前位置最大矩形宽w=min(w1,w2)+1,高h=min(h1,h2)
- *     如果该位置数字为0:
- *         当前位置最大矩形宽w=0,高h=0
+ * 采用动态规划的思想，创建保存每个位置能出现的最大矩形的宽的数组,即保存为柱状图的数据
+ * 遍历每一个保存的数据，从上往下计算以该位置为右下角的形成的最大矩形的面积，取最大值
 */
 /**
  * @param {character[][]} matrix
  * @return {number}
  */
 var maximalRectangle = function (matrix) {
-    var i, j, max = 0, save = [], w1, w2, h1, h2;
+    var i, j, Max = 0, save = [], w;
     for (i = 0; i < matrix.length; i++) {
         save.push(new Array(matrix[i].length));
     }
     for (i = 0; i < matrix.length; i++) {
         for (j = 0; j < matrix[i].length; j++) {
             if(matrix[i][j] == '1'){
-                w1 = i == 0 ? 1 : Math.min(save[i - 1][j][0],(j==0?1:save[i][j-1][0]+1));
-                h1 = i == 0 ? 1 : save[i - 1][j][1]+1;
-                w2 = j == 0 ? 1 : save[i][j-1][2]+1;
-                h2 = j == 0 ? 1 : Math.min(save[i][j-1][3],(i==0?1:save[i-1][j][3]+1));
-                w1 = w1==0?1:w1;
-                h2 = h2==0?1:h2;
-                save[i][j] = [w1, h1, w2, h2];
-                max = Math.max(max,w1*h1,w2*h2);
+                if(j>0&&matrix[i][j-1]=='1'){
+                    w = save[i][j-1]+1;
+                }else{
+                    w = 1;
+                }
+                save[i][j] = w;
             }else{
-                save[i][j] = [0, 0, 0, 0];
+                save[i][j] = 0;
             }
         }
     }
-    console.log(save)
-    return max;
+    for(i=0;i<matrix.length;i++){
+        for(j=0;j<matrix[i].length;j++){
+            if(save[i][j] != 0){
+                Max = Math.max(Max,calBarMax(i,j));
+            }
+        }
+    }
+    function calBarMax(x,y){
+        var max = 1, height = 1,width=Infinity;
+        while(x >= 0 && save[x][y] != 0){
+            width = Math.min(width,save[x][y]);
+            max = Math.max(width*height,max);
+            height ++;
+            x--;
+        }
+        return max;
+    }
+    return Max;
 };
 console.log(maximalRectangle([
     ["1","0","1","0","0"],
