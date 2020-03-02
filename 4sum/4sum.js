867396b25387fc66cb0061fb0913eca2a7eadba2
woaidouya123 <cfeigood@qq.com>
2020-03-02 19:48:55 +0800

    四数之和

---
+++ 4sum/4sum.js
@@ -0,0 +1,46 @@
+/**
+    题目： 四数之和
+    给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+
+    注意：
+
+    答案中不可以包含重复的四元组。
+
+    示例：
+
+    给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+
+    满足要求的四元组集合为：
+    [
+    [-1,  0, 0, 1],
+    [-2, -1, 1, 2],
+    [-2,  0, 0, 2]
+    ]
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function(nums, target) {
+    let len = nums.length, res = [];
+    nums.sort((a,b)=>a-b);
+    for(let i=0;i<len-3;i++){
+        for(let j=i+1;j<len-2;j++){
+            if(nums[i]+nums[j] > target && nums[j+1]>=0)break;
+            let start = j+1, end = len-1;
+            while(start < end){
+                let temp = nums[i] + nums[j] + nums[start] + nums[end];
+                if(temp === target){
+                    res.push([nums[i], nums[j], nums[start], nums[end]]);
+                }
+                while(temp >= target&&nums[end]===nums[--end]){}
+                while(temp < target&&nums[start]===nums[++start]){}
+            }
+            while(nums[j]==nums[j+1])j++;
+        }
+        while(nums[i]==nums[i+1])i++;
+    }
+    return res;
+};
+console.log(fourSum([1,-2,-5,-4,-3,3,3,5],-11))
