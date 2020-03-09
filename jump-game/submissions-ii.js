94828e83e1c077b87480709abda1d887c7c256e4
woaidouya123 <cfeigood@qq.com>
2020-03-09 15:07:33 +0800

    跳跃游戏 II

---
+++ jump-game/submissions-ii.js
@@ -0,0 +1,36 @@
+/**
+    题目：跳跃游戏 II
+    给定一个非负整数数组，你最初位于数组的第一个位置。
+
+    数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+    你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+
+    示例:
+
+    输入: [2,3,1,1,4]
+    输出: 2
+    解释: 跳到最后一个位置的最小跳跃数是 2。
+         从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    let len = nums.length;
+    let amount = len-1, count=0;
+    while(amount > 0){
+        let index = amount;
+        for(let i=0;i<amount;i++){
+            if(nums[i] >= amount-i){
+                index = i;
+                break;
+            }
+        }
+        count++;
+        amount = index;
+    }
+    return count;
+
+};
