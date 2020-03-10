4cd05b3594a7a67f32c9e9f25044ddeab4e8a8a6
woaidouya123 <cfeigood@qq.com>
2020-03-10 16:24:02 +0800

    二叉树的直径

---
+++ diameter-of-binary-tree/diameter-of-binary-tree.js
@@ -0,0 +1,40 @@
+/**
+题目：二叉树的直径
+给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过根结点。
+
+示例 :
+给定二叉树
+
+          1
+         / \
+        2   3
+       / \     
+      4   5    
+返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+
+注意：两结点之间的路径长度是以它们之间边的数目表示。
+
+*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
+    let max = 0;
+    function dp(node){
+        if(!node)return 0;
+        let left = dp(node.left),
+            right = dp(node.right);
+        max = Math.max(max,left+right);
+        return Math.max(left,right)+1;
+    }
+    dp(root);
+    return max;
+};
