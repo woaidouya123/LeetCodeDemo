cb0e597c9c1791487c4298c44850d78b49fba213
woaidouya123 <cfeigood@qq.com>
2020-10-15 22:31:29 +0800

    删除排序链表中的重复元素

---
+++ remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -0,0 +1,37 @@
+/**
+	题目：删除排序链表中的重复元素
+	给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+
+	示例 1:
+
+	输入: 1->1->2
+	输出: 1->2
+	示例 2:
+
+	输入: 1->1->2->3->3
+	输出: 1->2->3
+*/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let pre = head, node = pre&&pre.next;
+    while(node){
+        if(pre.val === node.val){
+            pre.next = node.next;
+        }else{
+            pre = node;
+        }
+
+        node = node.next;
+    }
+    return head;
+};
