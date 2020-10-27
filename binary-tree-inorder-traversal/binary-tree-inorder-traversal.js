bf092a16d47be08620e62a0628939f24e1752046
woaidouya123 <cfeigood@qq.com>
2020-10-27 23:37:32 +0800

    二叉树的中序遍历

---
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -0,0 +1,56 @@
+/**
+题目：二叉树的中序遍历
+给定一个二叉树，返回它的中序 遍历。
+
+示例:
+
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [1,3,2]
+
+*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// 递归
+// var inorderTraversal = function(root) {
+//     let res = [];
+//     let dfs = function(node){
+//         if(node){
+//             dfs(node.left);
+//             res.push(node.val);
+//             dfs(node.right);
+//         }
+//     }
+//     dfs(root);
+//     return res;
+// };
+// 迭代
+var inorderTraversal = function(root) {
+    const res = [];
+    const stk = [];
+    while (root || stk.length) {
+        while (root) {
+            stk.push(root);
+            root = root.left;
+        }
+        root = stk.pop();
+        res.push(root.val);
+        root = root.right;
+    }
+    return res;
+};
