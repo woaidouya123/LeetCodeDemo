cf022c540ccba1c3d3814b35439fd74b431668d1
woaidouya123 <cfeigood@qq.com>
2020-11-10 17:56:36 +0800

    旋转链表

---
+++ rotate-list/submissions.js
@@ -0,0 +1,53 @@
+/**
+题目：旋转链表
+给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+
+示例 1:
+
+输入: 1->2->3->4->5->NULL, k = 2
+输出: 4->5->1->2->3->NULL
+解释:
+向右旋转 1 步: 5->1->2->3->4->NULL
+向右旋转 2 步: 4->5->1->2->3->NULL
+示例 2:
+
+输入: 0->1->2->NULL, k = 4
+输出: 2->0->1->NULL
+解释:
+向右旋转 1 步: 2->0->1->NULL
+向右旋转 2 步: 1->2->0->NULL
+向右旋转 3 步: 0->1->2->NULL
+向右旋转 4 步: 2->0->1->NULL
+
+*/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+    let node = head, pre = null, len = 0;
+    if(head == null)return head;
+    while(node != null){
+        len++;
+        pre = node;
+        node = node.next;
+    }
+    pre.next = head;
+    let count = len - k%len - 1;
+    node = head;
+    while(count > 0){
+        count--;
+        node = node.next;
+    }
+    let next = node.next;
+    node.next = null;
+    return next;
+};
